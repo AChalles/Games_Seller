81d163bc5a5bba82a0d46f06bb317907fa7d27ad
Achalles <achalles32@aceproject.space>
2022-11-20 15:17:55 -0600

    migrates database to include genres table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_211332) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_211706) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_211332) do
     t.date "publish_date"
     t.decimal "price"
     t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
