5baa7343c1cb95c2a19526cefcf617ced503098b
Achalles <achalles32@aceproject.space>
2022-11-24 13:00:01 -0600

    adds active admin so admins can create and edit games/genres.

--- Gemfile
+++ Gemfile
@@ -27,6 +27,12 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem 'sassc-rails'
+
+gem "activeadmin"
+
+gem "devise"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :genres
-  resources :companies
   resources :games
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
