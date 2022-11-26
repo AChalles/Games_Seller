e3fa64b1159191ac34cd719d832eb82e72a6876b
Andrew Challes <achalles32@aceproject.space>
2022-11-26 14:17:41 -0600

    migrates database to include platforms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_182544) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_201511) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -54,6 +54,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_182544) do
   end
 
   create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "platforms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
