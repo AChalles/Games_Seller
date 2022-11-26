9e02025c5789100ee0a51d1142640457f6346216
Andrew Challes <achalles32@aceproject.space>
2022-11-26 14:13:25 -0600

    generates platform controller

---
+++ test/controllers/platform_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PlatformControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get platform_index_url
+    assert_response :success
+  end
+end
