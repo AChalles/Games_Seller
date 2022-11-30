54618c6837946dc09718124b8d363b8f2623ba54
Andrew Challes <achalles32@aceproject.space>
2022-11-30 00:13:04 -0600

    adds searchbar to application and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :genres
-  resources :games
+  resources :companies
+  #resources :games
+
+  resources :games, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
